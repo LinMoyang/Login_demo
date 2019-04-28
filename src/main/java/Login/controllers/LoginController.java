0ccb40458c74094a10018924ccf431bd8f5e8c64
“xiaolinzigege <1482228997@qq.com>
2019-04-28 09:50:03 +0800

    2019/4/28

--- src/main/java/Login/controllers/LoginController.java
+++ src/main/java/Login/controllers/LoginController.java
@@ -22,11 +22,14 @@ public class LoginController {
     @PostMapping("/")
     public String validateLoginInfo(Model model, @Valid LoginForm loginForm,Map<String,Object> map) {
         if(!StringUtils.isEmpty(loginForm.getUserName())&&"123456".equals(loginForm.getPassword())){
+            //Map和Model都可以往前台传送数据
+            //map.put("user",loginForm.getUserName());
             model.addAttribute("user", loginForm.getUserName());
             return "home";
         }//StringUtils是一个工具类,包含isEmpty等方法
         else {
-            map.put("msg","用户名密码错误！");
+            model.addAttribute("msg","yonghu");
+            //map.put("msg","用户名密码错误！");
             return "login";
         }
     }
